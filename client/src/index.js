4e81ea8dd2579a827aa2acaefa69fbaca1f71ede
Arslanov Rasul <stiflerovich@mail.ru>
2020-03-28 14:05:25 +0500

    фиксы сборки dev mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-import './scss/main.scss';
-import App from './App';
+import "./scss/main.scss";
+import App from "./App";
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(require("./routes"));
-app.use(express.static(path.resolve(__dirname, "views/public")));
 
 const SERVER_PORT = 5000;
 
